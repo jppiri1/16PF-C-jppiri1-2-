0fe01becf2f96c9911a9ed015316abc3c3050bbe
CAD Client <CAD Client>
2016-03-22 13:13:24 +0900

    주석사용

---
+++ ex02/ex02.py
@@ -0,0 +1,9 @@
+# A comment. this is so you can rea your program faster.
+# Anything after the # is ignore by python.
+
+print "I could have code i like this." # ans the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "this won`t run."
+
+print "this will run."
