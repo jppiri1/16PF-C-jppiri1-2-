a593fe0e293f75bb458a108d21865c92d83987bf
CAD Client <CAD Client>
2016-03-22 13:13:55 +0900

    사칙연산 사용

---
+++ ex03/ex03.py
@@ -0,0 +1,26 @@
+#-*-coding:cp949
+print "I will now count my chickens:"
+#닭의 수를 셀 것이다.
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 *3.0 % 4.0
+# "Hens" = 25+30/6, "Roosters" = 100-25*3%4
+print "Now I will count the eggs:"
+# 이제부터 달걀의 개수를 셀 것이다.
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# print 3+2+1-5+4%2-1/4+6
+print "Is it true that 3 + 2 < 5 - 7?"
+# "3+2<5-7"이 사실인가?
+print 3.0 + 2.0 < 5.0 - 7.0
+# "3+2<5-7"이 사실인지 아닌지 출력
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+# "3+2"의 값 출력
+# "5-7"의 값 출력
+print "Oh, that`s why it`s False."
+# "Oh, that`s why it`s False" 출력
+print "How about some more."
+# "How about some more." 출력
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0>= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
+# 각각의 문장이 사실인지 아닌지 출력
