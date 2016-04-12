6f3c5faaf413e536919e73ded422ec3d464cac82
CAD Client <CAD Client>
2016-04-12 10:20:42 +0900

    raw_input의 역할 아나콘다3 = raw_input -> input

---
+++ ex12/ex12.py
@@ -0,0 +1,7 @@
+#-*-coding:cp949
+age = input ("How old are you? ") #아나콘다2에서는 raw_input 아나콘다3에서는 input
+height = input ("How tall are you? ")
+weight = input ("How much do you weight? ")
+
+print ("So, you`re %r old, %r tall and %r heavy." % (
+    age, height, weight))
