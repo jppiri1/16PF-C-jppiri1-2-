364a6028bc82a4c273c7597a37b9b8f5b89fb7ef
CAD Client <CAD Client>
2016-05-24 11:44:37 +0900

    ex34

---
+++ ex34/ex34.py
@@ -0,0 +1,13 @@
+#-coding:utf8
+# http://learnpythonthehardway.org/book/
+
+animals = ['곰', '호랑이', '펭귄', '얼룩말']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+print("bear = %s" % bear)
+print("tiger = %s" % tiger)
+print("penguin = %s" % penguin)
+print("zebra = %s" % zebra)
